63597655ee5bfbf9eff779a45f3f3dedc1bd8459
roshnnkk <roshanakabd78@gmail.com>
2025-01-05 10:47:29 +0330

    responsive header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,13 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
 import { NavLink } from "react-router";
-import { CONTACT_ROUTE, HOME_ROUTE, LOGIN_ROUTE, RESERVE_ROUTE, ROOMS_ROUTE } from "../constant/routes";
+import {
+  CONTACT_ROUTE,
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  RESERVE_ROUTE,
+  ROOMS_ROUTE,
+} from "../constant/routes";
 import { AppBar, Box, Toolbar } from "@mui/material";
 import Buttons from "./buttons";
 
@@ -11,40 +17,41 @@ const Header = () => {
     { id: 2, name: "Login", to: LOGIN_ROUTE },
     { id: 3, name: "Booking", to: RESERVE_ROUTE },
     { id: 4, name: "Rooms", to: ROOMS_ROUTE },
-    { id: 5, name: "Contact", to: CONTACT_ROUTE }
-
-
-
-
+    { id: 5, name: "Contact", to: CONTACT_ROUTE },
   ];
 
   return (
-    // <div className="w-screen flex-row-reverse bg-transparent text-lg pt-3 text-white  flex justify-around ">
-    //   <div className=" xm:hidden ">
-    //     <MenuIcon />
-    //   </div>
-    //   <h1>ROYAL HOTEL</h1>
-
-    //   <div>
-    <div>
+    <>
+      <div className="md:hidden text-white flex justify-between py-2 px-7">
+        <h1>ROYAL HOTEL</h1>
+        <div>
+          <MenuIcon />
+        </div>
+      </div>
+      <div className="hidden md:block">
         <Box sx={{ flexGrow: 1, width: 1 }}>
-        <AppBar style={{ backgroundColor: 'transparent', boxShadow: 'none', }}>
-            <Toolbar style={{gap:"10px", justifyContent: "space-around" }}>
+          <AppBar style={{ backgroundColor: "transparent", boxShadow: "none" }}>
+            <Toolbar style={{ gap: "10px", justifyContent: "space-around" }}>
               <h1>Royal Hotel</h1>
-          <div className="flex gap-6">
-          {headerItems.map((items) => (
-                <NavLink key={items.id} to={items.to} className={({isActive})=>isActive ? "text-amber-500" : "text-yellow-100" }>
-                  {items.name}
-                </NavLink>
-                
-              ))}
-          </div>
-              <Buttons width="100px" text="Book now"/>
+              <div className="flex gap-6">
+                {headerItems.map((items) => (
+                  <NavLink
+                    key={items.id}
+                    to={items.to}
+                    className={({ isActive }) =>
+                      isActive ? "text-amber-500" : "text-yellow-100"
+                    }
+                  >
+                    {items.name}
+                  </NavLink>
+                ))}
+              </div>
+              <Buttons width="100px" text="Book now" />
             </Toolbar>
           </AppBar>
         </Box>
       </div>
-    
+    </>
   );
 };
 
